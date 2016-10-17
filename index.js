d6d1fe1a4539c87b7dc8dc9738ae184e2b19c9ee
Varun Gupta <vgupta16@gmail.com>
2016-10-17 15:25:31 -0400

    add http dep

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Bot = require('messenger-bot')
 const config = require(`./config/${process.env.NODE_ENV || 'production'}`);
+const http = requure('http');
 var bot_helper = require('./helpers/bot_helpers.js');
+
 
 let bot = new Bot(config.mBot);
 
